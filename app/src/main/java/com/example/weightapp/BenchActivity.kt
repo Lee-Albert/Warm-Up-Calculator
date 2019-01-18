4e49e2713b6a91ee0d1e9f3eb2db5de784b03977
Albert Lee <albert.d200@gmail.com>
2019-01-18 02:37:52 -0500

    adjusted application name

--- app/src/main/java/com/example/weightapp/BenchActivity.kt
+++ app/src/main/java/com/example/weightapp/BenchActivity.kt
@@ -1,4 +1,4 @@
-package com.example.weightapp
+package com.albert.weightapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
--- app/src/main/java/com/example/weightapp/MainActivity.kt
+++ app/src/main/java/com/example/weightapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.weightapp
+package com.albert.weightapp
 
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
